6ad120f403923c791bc9995fb9d6ef1bd1edae5e
Roman Prybudko <romanprybudko@gmail.com>
2018-11-07 17:20:50 +0200

    Add category controller. Create action index. Show hits

---
+++ controllers/AppController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 07.11.2018
+ * Time: 16:34
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class AppController extends Controller
+{
+
+}
