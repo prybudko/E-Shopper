8602d10d022a16fdd696dcd3fc7dfa06a9e02e15
Roman Prybudko <romanprybudko@gmail.com>
2018-12-03 17:54:51 +0200

    Update cart. Create actionDelItems, recalculate sum and qty. Added img for items

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -98,7 +98,7 @@ IEAppAsset::register($this);
                             <li><a href="#"><i class="fa fa-user"></i> Account</a></li>
                             <li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
                             <li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
-                            <li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>
+                            <li><a href="" onclick="return getCart()"><i class="fa fa-shopping-cart"></i> Cart</a></li>
                             <li><a href="login.html"><i class="fa fa-lock"></i> Login</a></li>
                         </ul>
                     </div>
@@ -127,7 +127,7 @@ IEAppAsset::register($this);
                                     <li><a href="shop.html">Products</a></li>
                                     <li><a href="product-details.html">Product Details</a></li>
                                     <li><a href="checkout.html">Checkout</a></li>
-                                    <li><a href="cart.html">Cart</a></li>
+                                    <li><a href="" id="show_cart">Cart</a></li>
                                     <li><a href="login.html">Login</a></li>
                                 </ul>
                             </li>
